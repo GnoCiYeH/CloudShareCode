50c0e56b280de461c3ccf82bdfd51490dd9dfb80
GnoCiYeH <118462160+GnoCiYeH@users.noreply.github.com>
2023-03-29 22:26:19 +0800

    数据包
    数据包的基本格式

--- Qt/CloudSharedCoding/package.cpp
+++ Qt/CloudSharedCoding/package.cpp
@@ -1,6 +1,39 @@
 #include "package.h"
+#include<QDebug>
 
-Package::Package()
+Package::Package(QByteArray data,int type)
+{
+    int dsize = data.size();
+    size = dsize+8;
+    if(dsize<=0)
+        return;
+
+    p_data.append(intToByteArr(type));
+    p_data.append(intToByteArr(dsize));
+    p_data.append(data);
+}
+
+QByteArray Package::intToByteArr(int num)
+{
+    QByteArray arr;
+    arr.resize(4);
+    arr[0] = (uchar)(0x000000ff & num);
+    arr[1] = (uchar)((0x0000ff00 & num)>>8);
+    arr[2] = (uchar)((0x00ff0000 & num)>>16);
+    arr[3] = (uchar)((0xff000000 & num)>>24);
+    return arr;
+}
+
+int Package::ByteArrToInt(QByteArray bytes,int offset)
+{
+    int addr = bytes[offset];
+    addr |= (bytes[offset+1] << 8);
+    addr |= (bytes[offset+2] << 16);
+    addr |= (bytes[offset+3] << 24);
+    return addr;
+}
+
+Package::~Package()
 {
 
 }
