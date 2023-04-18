04acc3867ae29a34bcfb5a36f9ed0409277cddd5
GnoCiYeH <118462160+GnoCiYeH@users.noreply.github.com>
2023-04-18 23:22:34 +0800

    项目云端编译运行优化

--- Server/CloudSharedCoding/TcpServer.cpp
+++ Server/CloudSharedCoding/TcpServer.cpp
@@ -10,6 +10,8 @@
 #include"uuid/uuid.h"
 #include<netinet/tcp.h>
 #include<netinet/in.h>
+#include<sys/types.h>
+#include<unistd.h>
 
 #define MAXCONNECTION 64
 #define EVENT_SIZE 20
@@ -678,7 +680,7 @@ void TcpServer::runProject(int sock_fd, char* data)
         pclose(fp);
     }
 
-    std::string runCmd = buildPath + "/bin/" + std::string(rows[0][0]) + std::string(" 2>&1");
+    /*std::string runCmd = buildPath + "/bin/" + std::string(rows[0][0]) + std::string(" 2>&1");
     if ((fp = popen(runCmd.c_str(), "r")) != NULL)
     {
         while (fgets(buf, sizeof(buf), fp) != NULL)
@@ -688,8 +690,74 @@ void TcpServer::runProject(int sock_fd, char* data)
             memset(buf, 0, sizeof(buf));
         }
         pclose(fp);
+    }*/
+
+    //读取进程的pipe
+    int outfd[2];
+
+    //向进程输入
+    int infd[2];
+
+    if (pipe(outfd) == -1)
+    {
+        std::string str = "Server create pipe error!";
+        Package pck(str.c_str(), (int)Package::ReturnType::SERVER_ERROR, str.size());
+        write(sock_fd, pck.getPdata(), pck.getSize());
+        ERROR_LOG(m_logger, "pipe error");
+        return;
+    }
+    int subWfd = outfd[1];
+    int mainRfd = outfd[0];
+
+    if (pipe(infd) == -1)
+    {
+        std::string str = "Server create pipe error!";
+        Package pck(str.c_str(), (int)Package::ReturnType::SERVER_ERROR, str.size());
+        write(sock_fd, pck.getPdata(), pck.getSize());
+        ERROR_LOG(m_logger, "pipe error");
+        return;
+    }
+    int mainWfd = infd[1];
+    int subRfd = infd[0];
+
+    int pid = fork();
+    if (pid == -1)
+    {
+        ERROR_LOG(m_logger, "fork error");
+    }
+    else if (pid == 0) //子进程
+    {
+        //运行用户项目代码
+
+        //重定向输入输出流
+        close(STDOUT_FILENO);
+        close(STDIN_FILENO);
+        dup2(subWfd, STDOUT_FILENO);
+        dup2(subRfd, STDIN_FILENO);
+
+        std::string exePath = buildPath + "/bin/" + std::string(rows[0][0]);
+        if (execlp(exePath.c_str(),NULL) == -1)
+        {
+            ERROR_LOG(m_logger, "run project error");
+        }
+
+        _Exit(-1);
+    }
+    else
+    {
+        //服务器代码
+        while (read(mainRfd,buf,sizeof(buf)!=0))
+        {
+            Package pck(buf, (int)Package::ReturnType::RUN_INFO, 1024);
+            write(sock_fd, pck.getPdata(), pck.getSize());
+            memset(buf, 0, sizeof(buf));
+        }
     }
 
+    close(mainRfd);
+    close(mainWfd);
+    close(subRfd);
+    close(subWfd);
     mysql_free_result(res);
     delete[] data;
 }
